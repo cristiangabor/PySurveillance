d9ec65f12a1ef71c303eb5493e3630370dddf086
cristiangabor <cristi26.gabor@gmail.com>
2016-10-14 09:24:53 +0300

    Enhaced the clock system

--- make_video_stream.py
+++ make_video_stream.py
@@ -35,11 +35,13 @@ class Record:   # BOOM! The Record class
         self.video_width=video_width
 
 
-    def make_thestreamhappen(self,video_height,video_width):
+    def make_thestreamhappen(self,video_height,video_width,video_name=''):
+
         # the brg24 format is used to keep the good coloration for the streamed video
-        f_format='brg24'                                        # defy the color scheme
-        dimension='{}x{}'.format(video_height,video_width)      # merge those two dimensions
-        FFMPEG='ffmpeg'                                         # defy the Ffmpeg arguments
+        f_format='brg24'
+        dimension='{}x{}'.format(video_height,video_width)
+        FFMPEG='ffmpeg'
+        video_name_complete=video_name + '.mkv'
 
         command=[FFMPEG,
                 '-re',                                # this argument is for updating the existent video file (optional)
@@ -47,7 +49,7 @@ class Record:   # BOOM! The Record class
                 '-c:v','rawvideo',                    # the video codec is not defined. It takes the raw format from frames
                 '-s',dimension,                       # the dimenisons of the frame
                 '-pix_fmt','bgr24',                   # pixel format
-                '-r','24',                            # frame rate
+                '-r','30',                            # frame rate
                 '-i','-',                             # it takes the video input from a pipe
                 '-an',                                # for now, it takes only the video from the opencv
                 '-c:v', 'libx264',                    # is making the video conversion into the specified fromat
@@ -55,24 +57,50 @@ class Record:   # BOOM! The Record class
                 'flv',                                # output format is flv
                 'rtmp://localhost/myapp/mystream']    # stream the video output
 
-        return command
+        command1=[FFMPEG,                            # command to recod in h264 codec on local disk
+                '-y',
+                '-f', 'rawvideo',
+                '-c:v','rawvideo',
+                '-s',dimension,
+                '-pix_fmt','bgr24',
+                '-r','30',
+                '-i','-',
+                '-an',
+                '-c:v','libx264',
+                '-b:v','5000k',
+                '-f','flv',
+                video_name_complete]
 
-    # Record with the gui video playe
+        return command,command1                   # I should return these baby's back
+
+    # Record with the gui video player
     def record_and_stream(self):
 
         # Print the process id on UNIX and WINDOW
         print ("\nThe id process:",os.getpid(),"started!\n")
         # Defy the codec and create VideoWriter object
         cap = cv2.VideoCapture(0)
-        fourcc = cv2.VideoWriter_fourcc(*self.video_format)
-        out = cv2.VideoWriter(self.video_name,fourcc, self.video_frame_rate, (self.video_height,self.video_width))
 
         # Memorate the time the video record started
         time_before=datetime.datetime.now()
 
+        # For the video name file #################################################################################
+        video_name=''                                                                                            ##
+        year=time_before.year                                                                                    ##
+        month=time_before.month                                                                                  ##
+        day=time_before.day                                                                                      ##
+        hour=time_before.hour                                                                                    ##
+        minute=time_before.minute                                                                                ##
+        video_name='From'+ str(day) + '|'+ str(month) + '|' + str(year)  + '-' + str(hour) + ':' +str( minute)   ##
+        ###########################################################################################################
 
-        command_to_start = self.make_thestreamhappen(self.video_height,self.video_width)
-        proces=sp.Popen(command_to_start, stdin=sp.PIPE, stderr=sp.PIPE)
+
+            # assign the video name with the date the video is recorded
+        command_to_stream,command_to_record = self.make_thestreamhappen(self.video_height,self.video_width,video_name)
+        proces_record=sp.Popen(command_to_record, stdin=sp.PIPE, stderr=sp.PIPE)
+
+        command_to_stream,command_to_record = self.make_thestreamhappen(self.video_height,self.video_width)
+        proces_stream=sp.Popen(command_to_stream, stdin=sp.PIPE, stderr=sp.PIPE)
 
 
         # Start frame recording
@@ -96,9 +124,10 @@ class Record:   # BOOM! The Record class
                 font = cv2.FONT_HERSHEY_SIMPLEX # Choose a font for the time string in the video
 
                 cv2.putText(frame,str(time_on_video_trunchated),(410,20), font, 0.6,self.time_color,self.time_thickness,cv2.LINE_AA) # Atache the time in the upper right of the video
-                proces.stdin.write(frame.tostring())
+                proces_stream.stdin.write(frame.tostring())
+                proces_record.stdin.write(frame.tostring())
 
-                out.write(frame)              # write the frames and saves them
+
                 cv2.imshow('frame',frame)     # start the video palyer from opencv
                 if self.check_time == 'minute':                                     # Checks if the time is set to minutes
                     past_time=time_before + datetime.timedelta(minutes=1)
@@ -120,9 +149,10 @@ class Record:   # BOOM! The Record class
 
         # Release everything if job is finished
         cap.release()
-        out.release()
-        proces.stdin.close()
-        proces.stderr.close()
+        proces_record.stdin.close()
+        proces_record.stderr.close()
+        proces_stream.stdin.close()
+        proces_stream.stderr.close()
         cv2.destroyAllWindows()  # Destroy the window for the player
 
     def record_and_show(self):
@@ -131,11 +161,24 @@ class Record:   # BOOM! The Record class
         print ("\nThe id process:",os.getpid(),'started!\n')
         # Defy the codec and create VideoWriter object
         cap = cv2.VideoCapture(0)
-        fourcc = cv2.VideoWriter_fourcc(*self.video_format)
-        out = cv2.VideoWriter(self.video_name,fourcc, self.video_frame_rate, (640,480))
+
 
         # Memorate the time the video record started
         time_before=datetime.datetime.now()
+
+        # For the video name file #################################################################################
+        video_name=''                                                                                            ##
+        year=time_before.year                                                                                    ##
+        month=time_before.month                                                                                  ##
+        day=time_before.day                                                                                      ##
+        hour=time_before.hour                                                                                    ##
+        minute=time_before.minute                                                                                ##
+        video_name='From'+ str(day) + '|'+ str(month) + '|' + str(year)  + '-' + str(hour) + ':' +str( minute)   ##
+        ###########################################################################################################
+
+        # assign the video name with the date the video is recorded
+        command_to_stream,command_to_record = self.make_thestreamhappen(self.video_height,self.video_width,video_name)
+        proces_record=sp.Popen(command_to_record, stdin=sp.PIPE, stderr=sp.PIPE)
 
         # Start frame recording
         while(True):
@@ -154,16 +197,16 @@ class Record:   # BOOM! The Record class
 
                 # Memorate the time for each frame
                 now=datetime.datetime.now()
-
-
-                font = cv2.FONT_HERSHEY_SIMPLEX # Choose a font for the time string in the video
-
+                # Choose a font for the time string in the video
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                # The clock in the video frame
                 cv2.putText(frame,str(time_on_video_trunchated),(410,20), font, 0.6,self.time_color,self.time_thickness,cv2.LINE_AA) # Atache the time in the upper right of the video
-                out.write(frame)              # write the frames and saves them
-                cv2.imshow('frame',frame)     # start the video palyer from opencv
+                proces_record.stdin.write(frame.tostring())
+
+                cv2.imshow('frame',frame)                                           # start the video palyer from opencv
                 if self.check_time == 'minute':                                     # Checks if the time is set to minutes
                     past_time=time_before + datetime.timedelta(minutes=1)
-                    if now > past_time:       # Checks if it is a different minute since the video recording started
+                    if now > past_time:                                             # Checks if it is a different minute since the video recording started
                         break                                                       # If that is true it is stoping the video recording
                 elif self.check_time == 'hour':
                     past_time=time_before + datetime.timedelta(hours=1)
@@ -181,7 +224,8 @@ class Record:   # BOOM! The Record class
 
         # Release everything if job is finished
         cap.release()
-        out.release()
+        proces_record.stdin.close()
+        proces_record.stderr.close()
         cv2.destroyAllWindows()  # Destroy the window for the player
 
 
@@ -195,7 +239,20 @@ class Record:   # BOOM! The Record class
         # Memorate the time the video record started
         time_before=datetime.datetime.now()
 
+        # For the video name file #################################################################################
+        video_name=''                                                                                            ##
+        year=time_before.year                                                                                    ##
+        month=time_before.month                                                                                  ##
+        day=time_before.day                                                                                      ##
+        hour=time_before.hour                                                                                    ##
+        minute=time_before.minute                                                                                ##
+        video_name='From'+ str(day) + '|'+ str(month) + '|' + str(year)  + '-' + str(hour) + ':' +str( minute) ##
+        ###########################################################################################################
 
+
+        # assign the video name with the date the video is recorded
+        command_to_stream,command_to_record = self.make_thestreamhappen(self.video_height,self.video_width,video_name)
+        proces_record=sp.Popen(command_to_record, stdin=sp.PIPE, stderr=sp.PIPE)
 
         # Start frame recording
         while(True):
@@ -217,7 +274,7 @@ class Record:   # BOOM! The Record class
                 font = cv2.FONT_HERSHEY_SIMPLEX # Choose a font for the time string in the video
                 cv2.putText(frame,str(time_on_video_trunchated),(410,20), font, 0.6,self.time_color,self.time_thickness,cv2.LINE_AA) # Atache the time in the upper right of the video
 
-
+                proces_record.stdin.write(frame.tostring())
 
                 if self.check_time == 'minute':                                     # Checks if the time is set to minutes
                     past_time=time_before + datetime.timedelta(minutes=1)
@@ -238,7 +295,8 @@ class Record:   # BOOM! The Record class
 
         # Release everything if job is finished
         cap.release()
-        out.release()
+        proces_record.stdin.close()
+        proces_record.stderr.close()
 
 
 
@@ -246,6 +304,11 @@ if __name__=="__main__":
     # Create the Record object and pass the global arguments to its class
     create_record=Record(video_format,video_frame_rate,time_color,time_thickness,video_name,check_time,video_height,video_width)
     #Start the Record process
+    #Get the path of the script
+    program_path=os.path.abspath('.')
+
+
+
     first_procees=Process(target=create_record.record_and_stream)
 
     start_recording=input("Start the video recording? (y/n):")
